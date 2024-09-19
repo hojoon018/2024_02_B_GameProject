d8f4ee11c5c985ac03c3b093da1e246c4f063489
hojoon018 <skewed018@gmail.com>
2024-09-19 10:24:30 +0900

    [Feat] Action Event Generic 예시

---
+++ Assets/Scripts/SimpleCallback.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCallback : MonoBehaviour
+{
+
+    private Action greetingAction;              //액션 선언
+
+    void Start()
+    {
+        greetingAction = SayHello;              //Action 함수 할당
+        PerformGreeting(greetingAction);
+    }
+
+    // Update is called once per frame
+    void SayHello()
+    {
+        Debug.Log("Hello, world!");
+    }
+
+    void PerformGreeting(Action greetingFunc)
+    {
+        greetingFunc?.Invoke();
+    }
+}
