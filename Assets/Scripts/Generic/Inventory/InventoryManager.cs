eaa36cba04458324e9456d3e6c098b7761130d1b
hojoon018 <skewed018@gmail.com>
2024-11-14 10:57:42 +0900

    [Feat] 조합 시스템 세팅

--- Assets/Scripts/Generic/Inventory/InventoryManager.cs
+++ Assets/Scripts/Generic/Inventory/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 //모든 아이템의 기본 인터페이스 interface 클래스 
@@ -11,6 +12,24 @@ public interface IItem
     string Name { get; }
     int ID { get; }
     void Use();
+}
+
+//CraftingMaterial 클래스 추가
+public class CraftingMaterial : IItem
+{
+    public string Name { get; private set; }
+    public int ID { get; private set; }
+
+    public CraftingMaterial(string name, int id)
+    {
+        Name = name;
+        ID = id;
+    }
+
+    public void Use()
+    {
+        Debug.Log($"This is a crafting material : {Name}");
+    }
 }
 
 //구체적인 아이템 클래스 (Weapon)
@@ -80,12 +99,37 @@ public class Inventory<T> where T : IItem
             Debug.Log($"Item: {item.Name} , ID : {item.ID}");
         }
     }
+
+    public void RemoveItems(int itemId, int amount)
+    {
+        int removed = 0;
+        for(int i = items.Count - 1; i >= 0; i--)
+        {
+            if (items[i].ID == itemId)
+            {
+                items.RemoveAt(i);
+                removed++;
+                if (removed >= amount)
+                    break;
+            }
+        }
+    }
+
+    public bool HasEnough(int itemId, int amount)           //아이템이 충분한지 검사
+    {
+        return GetItemCount(itemId) >= amount;
+    }
+
+    public int GetItemCount(int itemId)                     //아이템 카운트 함수
+    {
+        return items.Count(item => item.ID == itemId);
+    }
 }
 
 //인벤토리 Manager 
 public class InventoryManager : MonoBehaviour
 {
-    private Inventory<IItem> playerInventory;
+    private Inventory<IItem> playerInventory = new Inventory<IItem>();
     public int UseBagIndex;
 
     void Start()
@@ -95,6 +139,20 @@ public class InventoryManager : MonoBehaviour
         //아이템 추가 
         playerInventory.AddItem(new Weapon("Sword", 1, 10));
         playerInventory.AddItem(new HealthPotion("Small Potion", 2, 20));
+
+        playerInventory.AddItem(new CraftingMaterial("Iron Ingot", 101));               //ID 101 : 철 주괴
+        playerInventory.AddItem(new CraftingMaterial("Iron Ingot", 101));               //ID 101 : 철 주괴
+        playerInventory.AddItem(new CraftingMaterial("Wood", 102));                     //ID 102 : 나무
+
+        playerInventory.AddItem(new CraftingMaterial("Herb", 201));                     //ID 201 : 약초
+        playerInventory.AddItem(new CraftingMaterial("Herb", 201));                     //ID 201 : 철 주괴
+        playerInventory.AddItem(new CraftingMaterial("Water", 202));                    //ID 202 : 철 주괴
+    }
+
+    //인벤토리 접근자 메서드 추가
+    public Inventory<IItem> GetInventory()
+    {
+        return playerInventory;
     }
 
     private void Update()
